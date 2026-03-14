8b42c9a30933dbbd665de0b1a2d40639eb5b6dc7
Kojo Bailey <kojomolojo@gmail.com>
2026-03-14 17:09:30 +0000

    Finish tokeniser

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,6 @@
 open Calc
 
 let () =
-  match Tokenizer.tokenize " They call me the OCamel" with
+  match Tokenizer.tokenize "6.9 * -(7/3 + 2)" with
   | Ok tokens -> print_endline ([%show: Tokenizer.Token.t list] tokens)
   | Error err -> Printf.printf "Error: %s\n" err
--- lib/tokenizer.ml
+++ lib/tokenizer.ml
@@ -10,11 +10,28 @@ module Token = struct
   [@@deriving show]
 end
 
+let is_digit c = c >= '0' && c <= '9'
+let is_float_literal_char c = is_digit c || c == '.'
+
+let parse_num input =
+  let str = String.of_seq (List.to_seq (List.take_while is_float_literal_char input)) in
+  (float_of_string str, List.drop_while is_float_literal_char input)
+
 let tokenize input = 
   let chars = List.of_seq (String.to_seq input) in
-  let rec go = function
+  let rec token_map token cs = Result.map (fun ts -> token :: ts) (go cs)
+  and go = function
     | []        -> Ok []
     | ' ' :: cs -> go cs
-    | c :: cs   -> Error ("Invalid token: " ^ String.make 1 c)
+    | '(' :: cs -> token_map Token.ParenthesisOpen cs
+    | ')' :: cs -> token_map Token.ParenthesisClose cs
+    | '+' :: cs -> token_map (Token.Operator Plus) cs
+    | '-' :: cs -> token_map (Token.Operator Minus) cs
+    | '*' :: cs -> token_map (Token.Operator Asterisk) cs
+    | '/' :: cs -> token_map (Token.Operator Slash) cs
+    | c :: cs when is_digit c ->
+        let (num, remaining) = parse_num (c :: cs) in
+        token_map (Token.Number num) remaining
+    | c :: _ -> Error ("Invalid token: " ^ String.make 1 c)
   in
   go chars
