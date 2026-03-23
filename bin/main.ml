1a1e9072aa3e9ed2843a65c612b36716547f0a7b
Kojo Bailey <kojomolojo@gmail.com>
2026-03-23 19:39:41 +0000

    Finish functioning evaluator

--- bin/main.ml
+++ bin/main.ml
@@ -1,12 +1,26 @@
 open Calc
 
-let () =
-  match Tokenizer.tokenize "6.9 * -(7/3 + 2)" with
+open Printf
+
+let rec main () =
+  print_string ">>> ";
+  let input = read_line () in
+  match Tokenizer.tokenize input with
   | Ok tokens ->
       print_endline ([%show: Tokenizer.Token.t list] tokens);
       print_endline "";
       begin match Parser.parse tokens with
-        | Ok ast -> print_endline ([%show: Parser.Expression.t] ast)
-        | Error err -> Printf.printf "Error: %s\n" err
+        | Ok ast ->
+            print_endline ([%show: Parser.Expression.t] ast);
+            print_endline "";
+            begin match Evaluator.evaluate ast with
+              | Ok result ->
+                  print_endline (string_of_float result);
+                  main ()
+              | Error err -> printf "Error: %s\n" err; main ()
+            end
+        | Error err -> printf "Error: %s\n" err; main ()
       end
-  | Error err -> Printf.printf "Error: %s\n" err
+  | Error err -> printf "Error: %s\n" err; main ()
+
+let () = main ()
