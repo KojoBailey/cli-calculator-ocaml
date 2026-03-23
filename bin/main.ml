2f8d9da2c568ed63dd91253c28ea6c2ddba9b684
Kojo Bailey <kojomolojo@gmail.com>
2026-03-23 20:23:38 +0000

    Finish all the basic functionality

--- bin/main.ml
+++ bin/main.ml
@@ -2,25 +2,41 @@ open Calc
 
 open Printf
 
+let ( let* ) = Result.bind
+
+let show_debug = ref false
+
+let calculate (input : string) : (float, string) result =
+  let* tokens = Tokenizer.tokenize input in
+  if !show_debug then begin
+    print_endline "=== Tokens ===";
+    print_endline ([%show: Tokenizer.Token.t list] tokens);
+    print_endline ""
+  end;
+  let* ast = Parser.parse tokens in
+  if !show_debug then begin
+    print_endline "=== Abstract Syntax Tree ===";
+    print_endline ([%show: Parser.Expression.t] ast);
+    print_endline ""
+  end;
+  let* result = Evaluator.evaluate ast in
+  if !show_debug then
+    print_endline "=== Result ===";
+  Ok result
+
 let rec main () =
   print_string ">>> ";
   let input = read_line () in
-  match Tokenizer.tokenize input with
-  | Ok tokens ->
-      print_endline ([%show: Tokenizer.Token.t list] tokens);
-      print_endline "";
-      begin match Parser.parse tokens with
-        | Ok ast ->
-            print_endline ([%show: Parser.Expression.t] ast);
-            print_endline "";
-            begin match Evaluator.evaluate ast with
-              | Ok result ->
-                  print_endline (string_of_float result);
-                  main ()
-              | Error err -> printf "Error: %s\n" err; main ()
-            end
-        | Error err -> printf "Error: %s\n" err; main ()
-      end
-  | Error err -> printf "Error: %s\n" err; main ()
+  begin match calculate input with
+  | Ok result -> print_endline (string_of_float result)
+  | Error err -> printf "Error: %s\n" err
+  end;
+  if !show_debug then
+    print_endline "";
+  main ()
 
-let () = main ()
+let truth_values = ["true"; "True"; "t"; "T"; "1"; "y"; "Y"; "yes"; "Yes"]
+
+let () =
+  show_debug := Array.length Sys.argv > 1 && List.mem Sys.argv.(1) truth_values;
+  main ()
