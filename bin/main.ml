3d390737f6c2975f7e835ac3216d08d22ff1cf21
Kojo Bailey <kojomolojo@gmail.com>
2026-03-25 22:09:43 +0000

    Add ability to exit program gracefully

--- bin/main.ml
+++ bin/main.ml
@@ -5,13 +5,15 @@ open Printf
 let rec main () =
   print_string ">>> ";
   let input = read_line () in
-  begin match Calculator.calculate input with
-  | Ok result -> print_endline (string_of_float result)
-  | Error err -> printf "[ERROR] %s\n" err
-  end;
-  if !Calculator.show_debug then
-    print_endline "";
-  main ()
+  if input = ":q" then () else begin
+    begin match Calculator.calculate input with
+    | Ok result -> print_endline (string_of_float result)
+    | Error err -> printf "[ERROR] %s\n" err
+    end;
+    if !Calculator.show_debug then
+      print_endline "";
+    main ()
+  end
 
 let truth_values = ["true"; "True"; "t"; "T"; "1"; "y"; "Y"; "yes"; "Yes"]
 
